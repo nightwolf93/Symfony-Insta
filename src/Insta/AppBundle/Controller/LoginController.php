7a292394b145fa6e8a718612e5738a041c097abd
Nyura95 <alexis@MacBook-Air-de-Courapied.local>
2014-12-03 16:18:46 +0100

    Formulaire OK BIATCH

--- src/Insta/AppBundle/Controller/LoginController.php
+++ src/Insta/AppBundle/Controller/LoginController.php
@@ -9,6 +9,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 use Insta\AppBundle\Form\Type\RegistrationType;
 use Insta\AppBundle\Form\Data\ConnectionData;
+use Insta\AppBundle\Entity\Account;
 
 class LoginController extends Controller
 {
@@ -17,8 +18,24 @@ class LoginController extends Controller
     * @Template("InstaAppBundle:Default:login.html.twig")
     */
     public function indexAction(){
+        $request = $this->getRequest();
         
-        $form = $this->createForm(new RegistrationType(), new ConnectionData());
+        $account = new Account();
+        $formBuilder = $this->createFormBuilder($account);
+        
+        $formBuilder
+                ->add('email', 'text')
+                ->add('password', 'text')
+                ->add('Login', 'submit');
+        
+        $form = $formBuilder->getForm();
+        $form->handleRequest($request);
+        
+        if($form->isValid()){
+            
+            return $this->redirect($this->generateUrl('home'));
+        }
+        
         return array('form' => $form->createView());
     }
     
