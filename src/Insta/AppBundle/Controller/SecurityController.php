0fafadd91b923e937be10f67764275cebdaf9231
Nenex <unbekannt67@gmail.com>
2014-12-04 12:14:50 +0100

    secure

---
+++ src/Insta/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Insta\AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContext;
+class SecurityController extends Controller {
+
+    /**
+     * @Route("/login", name="login")
+     * @Template()
+     */
+    public function loginAction(Request $request) {
+        $session = $request->getSession();
+
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(
+                    SecurityContext::AUTHENTICATION_ERROR
+            );
+        } else {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        }
+
+
+        return array(
+            'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+            'error' => $error,
+        );
+    }
+    
+    /**
+     * @Route("/login", name="login_check")
+     */
+    public function loginCheckAction(){
+        return array();
+        
+    }
+
+}
